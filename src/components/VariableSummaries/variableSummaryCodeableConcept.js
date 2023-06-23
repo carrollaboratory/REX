b63d7ebdfbe0454b1211a1b05bd0036468ffb3ad
Yelena Cox <yelenacox@gmail.com>
2023-06-23 12:25:08 -0500

    implemented variable summary UI improvements

--- src/components/VariableSummaries/variableSummaryCodeableConcept.js
+++ src/components/VariableSummaries/variableSummaryCodeableConcept.js
@@ -1,23 +1,26 @@
 import "./variableSummary.css";
+import StopSign from "../../images/stop-sign.png";
 
 export const VariableSummaryCodeableConcept = ({ variableData }) => {
   return (
     <>
       {variableData ? (
         <>
-          <table className="variable-summary-table-codeable">
+          <table className="variable-summary-table-codeable variable-summary-table">
             {variableData?.[0]?.resource?.component?.map((c) => (
               <tr key={c?.resource?.id}>
-                <div className="variable-summary-head-codeable">
+                <td className="empty-column"></td>
+
+                <div className="variable-summary-head-codeable variable-summary-head">
                   {" "}
                   <thead>
-                    <th className="variable-summary-head-cell-codeable">
+                    <th className="variable-summary-head-cell-codeable variable-summary-head-cell">
                       {c?.code?.coding?.[0]?.display}
                     </th>
                   </thead>
                 </div>
                 <td>
-                  <div className="variable-summary-cell-codeable">
+                  <div className="variable-summary-cell-codeable variable-summary-cell">
                     {c?.valueInteger}
                   </div>
                 </td>
@@ -26,7 +29,23 @@ export const VariableSummaryCodeableConcept = ({ variableData }) => {
           </table>
         </>
       ) : (
-        <div className="no-data">No data found</div>
+        <div className="no-data-div">
+          <table className="no-data-table">
+            <tr>
+              <td className="empty-column"></td>
+              <td className="no-data-cell">
+                <div>
+                  <img
+                    src={StopSign}
+                    className="stop-sign"
+                    alt="Stop sign icon"
+                  />
+                </div>
+                <div>No data found.</div>
+              </td>
+            </tr>
+          </table>
+        </div>
       )}
     </>
   );
