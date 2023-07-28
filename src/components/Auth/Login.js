5a65bf8c17c4fc52fbcdee406aef3c372862d521
Yelena Cox <yelenacox@gmail.com>
2023-07-27 20:30:44 -0500

    first draft of login page for gregor

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,5 +1,10 @@
 import { useContext } from "react";
 import { authContext } from "../../App";
+import AnvilLogo from "../../images/anvil.png";
+import AnvilSmallLogo from "../../images/AnVIL_Little_Logo.png";
+import GoogleLoginButton from "../../images/btn_google_signin_light_focus_web.png";
+import "./login.css";
+import { Link } from "react-router-dom";
 
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
 
@@ -33,9 +38,41 @@ export const Login = () => {
   };
 
   return (
-    <div>
-      <button onClick={handleSignIn}>Sign in with Google</button>
-      {/* <button onClick={() => handleSignOut()}>Sign Out</button> */}
-    </div>
+    <>
+      <div className="login-wrapper">
+        <div className="left-side">
+          <img className="anvil-logo" alt="AnVIL logo" src={AnvilLogo} />
+        </div>
+        <div className="right-side">
+          <div className="login-gregor">AnVIL Data Resource Portal:</div>
+          <div className="login-gregor">GREGoR Consortium Workspace</div>
+          <div>
+            <img
+              className="login-button"
+              alt="Google loggin button"
+              onClick={handleSignIn}
+              src={GoogleLoginButton}
+            />
+          </div>
+          {/* <div className="login-trouble-div"> */}
+          <div className="login-trouble">
+            <p className="login-trouble-question">Having trouble logging in?</p>
+            Contact your access manager,<br></br>
+            <a href="mailto:bheavner@uw.edu" className="login-link">
+              Ben Heavner
+            </a>
+            , for further assistance.
+          </div>
+
+          <div className="anvil-small-logo-div">
+            <img
+              className="anvil-small-logo"
+              alt="Small AnVIL logo"
+              src={AnvilSmallLogo}
+            />
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
